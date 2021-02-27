bf735bd05c9b420738a0b2c6381ba46be198d88f
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-02-26 20:33:13 -0500

    quick fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
         <h2>Poop</h2>
-      </header>
+
     </div>
   );
 }
