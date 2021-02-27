44d0ac14a412572b25f0cf4654bc6e0c9dc52da1
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-02-26 20:47:22 -0500

    finally updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import LoginForm from './LoginForm'
+import RegisterForm from './RegisterForm'
+
+
 
 function Login() {
   return (
     <div>
       <h2>Loginingggggggg</h2>
+
+      {/* 🤞🐱‍🐉🎂 */}
+      <LoginForm />
+      <RegisterForm />
+
     </div>
   )
 }
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterForm() {
+  return (
+    <div>
+      <h2>Register form</h2>
+    </div>
+  )
+}
+
+export default RegisterForm
