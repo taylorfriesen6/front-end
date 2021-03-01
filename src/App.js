6e09fb3d8c1dea8551df87e5b8fea17c17e594b3
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-01 15:07:47 -0800

    fixed a typo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route exact path="/" component={Home}/>
       <Route path="/login" component={Login}/>
       <Route path="/signup" component={SignUp}/>
-      <Route path="/edit" component={EditRecipe/>
+      <Route path="/edit" component={EditRecipe}/>
       <Route path="/display" component={DisplayPage}/>
     </div>
   );
