e9b3a5f480b126044e34bfb644e115dd7e4b52e2
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-01 12:36:26 -0800

    recipe display working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 
+import DisplayPage from './components/DisplayPage'
+
 function App() {
   return (
     <div className="App">
       
-        <h2>Poop</h2>
+        {/* Including  the DisplayPage component for testing purposes*/}
+        <DisplayPage />
 
     </div>
   );
--- src/components/DisplayPage.js
+++ src/components/DisplayPage.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
 function DisplayPage() {
+
+  const recipe = {
+    title: 'Chocolate Cake',
+    category: 'cake, dessert,chocolate',
+    source: 'online',
+    ingredients: 'chocolate, flour, eggs, sugar, milk',
+    instructions: 'mix flour, eggs, sugar, and milk, melt chocolate and mix in, then bake.',
+    user_id: 1
+  };
+
+  const ingredientsArray = recipe.ingredients.split(',');
+
   return (
     <div>
-      <h2>Edit Page</h2>
+      <h1>{recipe.title}</h1>
+      {/* the links on the following line should be replaced with React Links once routing is set up */}
+      <a href='#'>Edit Recipe</a> <a href='#'>Delete Recipe</a>
+      <p>categories: <em>{recipe.category}</em></p>
+      <p>source: {recipe.source}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        {ingredientsArray.map(ingredient => {
+          return <li>{ingredient}</li>;
+        })}
+      </ul>
+      <h2>Directions</h2>
+      <p>{recipe.instructions}</p>
     </div>
   )
 }
